10748f74cada4a182d772e754586338b3508f37b
Rajkumar P <raj@diskodev.com>
2018-05-03 08:22:54 +0530

    Concurrent programs workings

--- ParallelMOOC/Java/Workings/src/main/java/edu/coursera/parallel/App.java
+++ ParallelMOOC/Java/Workings/src/main/java/edu/coursera/parallel/App.java
@@ -8,22 +8,7 @@ public class App
 {
     public static void main(String[] args)
     {
-        long[] numbers = new long[10000];
-        for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = 5;
-        }
-
-        Parallels p = new Parallels();
-        long sum = p.seqArraySum(numbers);
-
-        System.out.println(sum);
-
-        numbers = new long[10000];
-        for (int i = 0; i < numbers.length; ++i) {
-            numbers[i] = 5;
-        }
-
-        sum = p.parArraySum(numbers);
-        System.out.println(sum);
+        ConcurrentPrograms concurrentPrograms = new ConcurrentPrograms();
+        concurrentPrograms.scheduleTaskInExecutorService();
     }
 }
---
+++ ParallelMOOC/Java/Workings/src/main/java/edu/coursera/parallel/ConcurrentPrograms.java
@@ -0,0 +1,100 @@
+package edu.coursera.parallel;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class ConcurrentPrograms {
+    void runTaskInNewThread()
+    {
+        Runnable task = () -> {
+            String threadName = Thread.currentThread().getName();
+            String groupName = Thread.currentThread().getThreadGroup().getName();
+            System.out.println("Hello, " + threadName + ", from " + groupName);
+        };
+
+        task.run();
+
+        Thread newTask = new Thread(task);
+        newTask.start();
+
+        System.out.println("done.");
+    }
+
+    void runTaskInExecutorService()
+    {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(() -> {
+            String threadName = Thread.currentThread().getName();
+            String groupName = Thread.currentThread().getThreadGroup().getName();
+            System.out.println("Hello, " + threadName + ", from " + groupName);
+        });
+
+        System.out.println("done.");
+        executorService.shutdown();
+    }
+
+    void runTaskInExecutorServiceAndPrintReturnedValue()
+    {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Integer> int_value = executorService.submit(() -> {
+            String threadName = Thread.currentThread().getName();
+            String groupName = Thread.currentThread().getThreadGroup().getName();
+            System.out.println("Hello, " + threadName + ", from " + groupName);
+
+            return 123;
+        });
+
+        System.out.println("Future done? - " + int_value.isDone());
+        try {
+            String threadName = Thread.currentThread().getName();
+            String groupName = Thread.currentThread().getThreadGroup().getName();
+            System.out.println("Hello, " + threadName + ", from " + groupName);
+
+            System.out.println("Getting future value - " + int_value.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("done.");
+        executorService.shutdown();
+    }
+
+    void runMultipleTaskInExecutorServiceAndPrintReturnedValue() {
+        ExecutorService executorService = Executors.newWorkStealingPool();
+        List<Callable<String>> callables = Arrays.asList(
+                () -> "Task1",
+                () -> "Task2",
+                () -> "Task3"
+        );
+
+        try {
+            executorService.invokeAll(callables)
+                    .stream()
+                    .map(future -> {
+                        try {
+                            return future.get();
+                        } catch (Exception e) {
+                            throw new IllegalStateException();
+                        }
+                    })
+                    .forEach(System.out::println);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void scheduleTaskInExecutorService()
+    {
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+        int initialDelay = 0;
+        int period = 5;
+
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            System.out.println("Scheduling : " + System.nanoTime());
+        }, initialDelay, period, TimeUnit.SECONDS);
+    }
+}
